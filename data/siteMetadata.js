7d2ce2d6641dc392af95d424f901de0d248da572
Subash <154237809+subashbaniyaa@users.noreply.github.com>
2025-08-26 06:48:46 +0000

    Initial commit

---
+++ data/siteMetadata.js
@@ -0,0 +1,20 @@
+const siteMetadata = {
+  title: "Subash",
+  author: "Subash Baniya",
+  headerTitle: "Subash",
+  description: "Subash's site",
+  language: "en-us",
+  siteUrl: "https://subash-baniya.com.np",
+  siteRepo: "https://github.com/subashbaniyaa/subash-blog",
+  siteLogo: "/static/images/eren.jpeg",
+  image: "https://pbs.twimg.com/profile_images/1853846849989550081/Wx-52OPe_400x400.jpg",
+  socialBanner: "/static/images/subash.jpeg",
+  email: "hi@subash-baniya.com.np",
+  github: "https://github.com/subashbaniyaa",
+  twitter: "https://twitter.com/",
+  linkedin: "https://www.linkedin.com/",
+  codepen: "https://codepen.io/",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
